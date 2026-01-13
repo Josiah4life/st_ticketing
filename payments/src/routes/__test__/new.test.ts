84335b8a2d0372a9ce8ab57c0073a8139c21c972
Josiah <93512794+Josiah4life@users.noreply.github.com>
2026-01-13 05:34:36 +0100

    add startup message to order service

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -6,8 +6,6 @@ import { Order } from '../../models/order';
 import { stripe } from '../../stripe';
 import { Payment } from '../../models/payments';
 
-// jest.mock("../../stripe")
-
 it('returns a 404 when purchasing an order that does not exist', async () => {
     await request(app)
         .post('/api/payments')
